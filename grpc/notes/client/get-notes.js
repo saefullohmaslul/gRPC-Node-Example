a22cf0ce28ba2be7536bf5a982480771c4e82ff5
Saefulloh Maslul <maslulsaefulloh@gmail.com>
2020-04-24 14:32:27 +0700

    enhancement: change method to async await

--- grpc/notes/client/get-notes.js
+++ grpc/notes/client/get-notes.js
@@ -1,8 +1,18 @@
 const client = require('.')
 
-client.list({}, (error, notes) => {
-  if (!error) {
-    return console.log(notes)
+const getNotes = async () => {
+  try {
+    const notes = await new Promise((resolve, reject) => {
+      client.list({}, (error, notes) => {
+        if (!error) resolve(notes)
+        reject(error)
+      })
+    })
+
+    console.log(notes)
+  } catch (error) {
+    console.log(error)
   }
-  return console.log(error)
-})
+}
+
+getNotes()
