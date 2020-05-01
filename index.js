d0314fa06a181df0675fc9804120ca9d627d81bd
Saefulloh Maslul <maslulsaefulloh@gmail.com>
2020-05-01 12:27:29 +0700

    change port and name package

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const app = require('./app')
 const notesServer = require('./grpc/notes/server')
 
-app.listen(8080, () => {
-  console.log(`App running on port: 8080`)
+app.listen(5000, () => {
+  console.log(`App running on port: 5000`)
 })
 
 notesServer.start()
